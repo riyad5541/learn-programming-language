c6a0185978d3c8d14833ed0267ce04cd58287c10
riyad5541 <abdullahalriyad5541@gmail.com>
2022-10-26 15:51:03 +0600

    add navbar

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import LeftSideNav from '../LeftSideNav/LeftSideNav';
 
 const Header = () => {
     return (
         <div>
-            this is header
+            <Navbar collapseOnSelect className='mb-4' expand="lg" bg="light" variant="light">
+                <Container>
+                    <div>
+                    <img src="" alt="" />
+                    <Navbar.Brand href="#home">Language For Code</Navbar.Brand>
+                    </div>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#features">Courses</Nav.Link>
+                            <Nav.Link href="#pricing">FAQ</Nav.Link>
+                            <Nav.Link href="#deets">Blog</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link eventKey={2} href="#memes">
+                                Dank memes
+                            </Nav.Link>
+                        </Nav>
+                        <div className='d-lg-none'>
+                            <LeftSideNav></LeftSideNav>
+                        </div>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftSideNav = () => {
+    const [categories,setCategories] = useState([]);
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/course-categories')
+        .then(res => res.json())
+        .then(data =>setCategories(data))
+    },[])
     return (
         <div>
-            <h2>leftside navs</h2>
+            <h2>All Course List :{categories.length}</h2>
+            <div>
+                {
+                    categories.map(category => <p key={category.id}>
+                        <Link to={`/categories/${category.id}`}>{category.name}</Link>
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
